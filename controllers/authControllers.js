7f59bb9c1f270f8e3dcb86bd68ef9f5be5df376a
Jorge <jemc0810@gmail.com>
2022-07-15 19:51:47 -0500

    first deploy

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -6,3 +6,28 @@ exports.signUp = (req, res) => {
     .then(user => res.status(201).json({user}))
     .catch(err => res.status(500).json({err}))
 }
+
+exports.login = (req, res, next) => {
+    const {user} = req
+    const [header, payload, signature] = createToken(user)
+
+    
+    const response = {
+        user,
+        
+    }
+
+    res.status(200).json({user})
+}
+
+exports.loggedUser = (req, res, next) => {
+    const {user} = req
+    console.log(user)
+    res.status(200).json({user})
+}
+
+exports.logout = (req, res, next) =>{
+    res.clearCookie('headload')
+    res.clearCookie('signature')
+    res.status(200).json({msg: 'saliste chido'})
+}
